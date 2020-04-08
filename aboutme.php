b61d2da3bd5fdacfecac90b70c0b3ee784c3bfe5
fauzanmaulana <fauzanfm735@gmail.com>
2020-04-08 19:44:57 +0700

    first commit

---
+++ aboutme.php
@@ -0,0 +1,7 @@
+<?php
+    echo 
+    "Nama : Fauzan Maulana<br>
+     Kelas : X RPL 1<br>
+     Asal Sekolah : SMK Taruna Bhakti Depok<br>
+     Alamat Rumah : Jl.Pekapuran, Gg.Bungur, RT.03/02, NO.58";
+?>
